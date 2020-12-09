0307646df32847e80e6d80af00f96a26d6f9988a
Alejandro Toledano <a.toledanoh@gmail.com>
2020-12-09 01:53:06 -0400

    renaming of the assignment

---
+++ ExtraAssignment/Object.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtraAssignment
+{
+    public class Object : IObject
+    {
+        public void DoSomething()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ExtraAssignment/ObjectFactory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtraAssignment
+{
+    public class ObjectFactory
+    {
+        public Object Create()
+        {
+            return new Object();
+        }
+
+    }
+}
